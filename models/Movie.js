79400e430b00be7479ad12358d285e44828fe31b
Ryan Dunton <ryan_dunton>
2018-11-05 16:32:23 -0500

    updating movie chema

--- models/Movie.js
+++ models/Movie.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 
-const Movie = new Schema({
+const movieSchema = new Schema({
   title: String,
   watched: { type: Boolean, default: false },
+  _user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
-module.exports = Movie;
+mongoose.model('movie', movieSchema);
